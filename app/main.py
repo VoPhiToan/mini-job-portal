9a6cac3a3657ea6cd2356cd0d64821560416112a
VoPhiToan <your-email@gmail.com>
2026-08-29 20:32:42 +0700

    Prepare backend for production deployment

--- app/main.py
+++ app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "https://mini-job-portal.pages.dev",
     ],
     allow_credentials=False,
     allow_methods=["*"],
